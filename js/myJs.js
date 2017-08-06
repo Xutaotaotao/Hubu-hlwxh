7f0c048a47ea1c00022ca0d5eed99474627a0071
xutaotaotao <xutaotao1108@outlook.com>
2017-08-06 23:32:17 +0800

    导航栏改变。

---
+++ js/myJs.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 徐涛焘 on 2017/8/6.
+ */
+(function(){
+    pos = 0;
+    ticking = false;
+    var header = document.querySelector(".navbar-inverse");
+    window.addEventListener("scroll", function(e){
+        pos = window.scrollY;
+        if(pos > 100&&!ticking){
+            header.classList.add("scrolledDown");
+            ticking = true;
+        }
+        if(pos < 100 && ticking){
+            header.classList.remove("scrolledDown");
+            ticking = false;
+        }
+    });
+})();
