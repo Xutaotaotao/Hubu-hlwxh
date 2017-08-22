9b976b6556bb6d6a9a1e0d0a305081e2a18ee7d4
xutaotaotao <xutaotao1108@outlook.com>
2017-08-22 23:00:39 +0800

    Signed-off-by: xutaotaotao <xutaotao1108@outlook.com>

--- page-template/page-galler.php
+++ page-template/page-galler.php
@@ -6,6 +6,39 @@ Template Name:相册
 <?php 
 get_header(); 
 ?>
+<div id="single">
+    <div class="container">
+        <?php 
+        if(have_posts()) :
+         ?>
+        <?php 
+        while(have_posts()) : the_post();
+        ?>
+        <?php 
+		echo [FinalTilesGallery id='1']
+         ?>
+        <?php 
+        endwhile; 
+        ?>
+        <div class="single-guide">
+            <nav class="navbar navbar-default navbar-fixed-bottom">
+                <p class="text-center">
+                <span> 上一篇：</span>
+                    <?php 
+                    previous_post_link('%link','%title');
+                     ?>
+                <span>下一篇：</span>
+                    <?php 
+                    next_post_link('%link','%title');
+                    ?>
+                </p>
+            </nav>
+        </div>
+        <?php else : ?>
+        <h2>Not Found</h2>
+        <?php endif; ?>
+    </div>
+</div>
 <footer id="wx-footer">
     <div class="container">
         <div class="row">
